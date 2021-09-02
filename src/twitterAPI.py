d5aa2b677e4e3f231ea8c940e9f18b00278148ba
Tailes14 <54458979+Tailes14@users.noreply.github.com>
2021-09-02 17:49:09 -0400

    basic twitter bot working

--- src/twitterAPI.py
+++ src/twitterAPI.py
@@ -1,7 +1,15 @@
-exec(open("spotifyAPI.py").read())
+from TwitterAPI import TwitterAPI
+import requests
+import configT
+
+
+
 
 def main():
-    pass
+    api = TwitterAPI(configT.consumerKey, configT.consumerSecret, configT.accessToken, configT.accessSecret)
+
+    r = api.request('statuses/update', {'status':'This is a tweet!'})
+    print(r.status_code)
 
 if __name__ == '__main__':
     main()
