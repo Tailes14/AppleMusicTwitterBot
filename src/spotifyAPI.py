63480f49b3ebe64ec148cd2ba56eacd010799c64
Tailes14 <54458979+Tailes14@users.noreply.github.com>
2021-09-02 18:46:09 -0400

    added a wait if no music is being played

--- src/spotifyAPI.py
+++ src/spotifyAPI.py
@@ -6,7 +6,6 @@ from TwitterAPI import TwitterAPI
 import configT
 
 
-
 SPOTIFY_ACCESS_TOKEN = configS.OAuthToken
 GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
 
@@ -14,7 +13,11 @@ def get_current_track(access_token):
     response = requests.get(GET_CURRENT_TRACK_URL, 
     headers={"Authorization": f"Bearer {access_token}"})
 
-    resp_json = response.json()
+
+    try:
+        resp_json = response.json()
+    except:
+        return
 
     track_id = resp_json['item']['id']
     track_name = resp_json['item']['name']
@@ -40,6 +43,10 @@ def tweet(api, info):
 def main():
     # running once to get the current song being played
     current_track_info = get_current_track(SPOTIFY_ACCESS_TOKEN)
+    if current_track_info == None:
+        print("No music being played, waiting...")
+        time.sleep(3)
+        main()
     currName = current_track_info['name']
     pprint(current_track_info, indent=4)
 
@@ -50,6 +57,9 @@ def main():
     # then looping and checking for a new song name being played
     while True:
         current_track_info = get_current_track(SPOTIFY_ACCESS_TOKEN)
+        if current_track_info == None:
+            print("No music being played, waiting...")
+            main()
         if current_track_info['name'] != currName:
             pprint(current_track_info, indent=4)
             currName = current_track_info['name']
